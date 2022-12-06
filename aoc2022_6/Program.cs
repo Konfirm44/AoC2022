76fc4ae130ef0560753eef101fabd36ce12b002d
Konfirm44 <tomsitek@gmail.com>
2022-12-06 17:31:21 +0100

    day 6

---
+++ aoc2022_6/Program.cs
@@ -0,0 +1,16 @@
+﻿var line = "";
+var markerLength = 14; //4;
+
+while ((line = Console.ReadLine()) is not null)
+{
+    Console.WriteLine(line);
+    var startMarker = Enumerable.Range(markerLength, line.Length - markerLength + 1)
+        .Select(i => line[(i - markerLength)..i])
+        .Select(s => s.Distinct().Count())
+        .ToList()
+        .FindIndex(c => c == markerLength)
+        + markerLength;
+    Console.WriteLine(startMarker);
+}
+
+Console.WriteLine();
